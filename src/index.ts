0a01c9b936d8d90e85290a975dcbaa1a1769f53a
Jörn Heinemann <joernheinemann@gmx.de>
2022-08-24 08:23:05 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export {SentcOptions} from "./Sentc";
+export {StorageInterface} from "./core";
+export {SymKey} from "./crypto/SymKey";
+export {Group} from "./Group";
+export {User} from "./User";
+
+import {Sentc} from "./Sentc";
+
+export default Sentc;
