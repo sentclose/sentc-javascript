a73f9c16fa28ac940e11164389bfba4e02ced892
Jörn Heinemann <joernheinemann@gmx.de>
2022-09-11 17:36:52 +0200

    fixed wrong public key id

--- src/User.ts
+++ src/User.ts
@@ -58,7 +58,7 @@ export async function getUser(deviceIdentifier: string, user_data: UserData)
 		storage.set(USER_KEY_STORAGE_NAMES.userData + "_id_" + deviceIdentifier, store_user_data),
 		storage.set(USER_KEY_STORAGE_NAMES.actualUser, deviceIdentifier),
 		//save always the newest public key
-		storage.set(USER_KEY_STORAGE_NAMES.userPublicKey + "_id_" + user_data.user_id, {key: user_data.user_keys[0].exported_public_key, id: user_data.user_id})
+		storage.set(USER_KEY_STORAGE_NAMES.userPublicKey + "_id_" + user_data.user_id, {key: user_data.user_keys[0].exported_public_key, id: user_data.user_keys[0].group_key_id})
 	]);
 
 	return new User(Sentc.options.base_url, Sentc.options.app_token, user_data, deviceIdentifier);
