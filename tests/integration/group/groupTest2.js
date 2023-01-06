33e8be8107c1906b3a6187e994c12ae33040f546
Jörn Heinemann <joernheinemann@gmx.de>
2023-01-06 15:05:39 +0100

    fixed wrong integration tests after group invite

--- tests/integration/group/groupTest2.js
+++ tests/integration/group/groupTest2.js
@@ -120,29 +120,29 @@ describe("Group test 2", () => {
 
 	it("should invite a group as member", async function() {
 		//connect group 1 with the new group
-		await group2.inviteGroupAuto(group.data.group_id);
+		await connected_group.inviteGroupAuto(group2.data.group_id);
 	});
 
 	it("should access the group after invite", async function() {
-		const group_c = await group.getConnectedGroup(group2.data.group_id);
+		const group_c = await group2.getConnectedGroup(connected_group.data.group_id);
 
-		chai.assert.equal(group_c.data.access_by_group_as_member, group.data.group_id);
+		chai.assert.equal(group_c.data.access_by_group_as_member, group2.data.group_id);
 	});
 
 	it("should send join req from group 2 to the new group", async function() {
-		await group1.groupJoinRequest(group2.data.group_id);
+		await group1.groupJoinRequest(connected_group.data.group_id);
 
 		//get the req in the sent req list
 
 		const joins = await group1.sentJoinReq();
 
 		chai.assert.equal(joins.length, 1);
-		chai.assert.equal(joins[0].group_id, group2.data.group_id);
+		chai.assert.equal(joins[0].group_id, connected_group.data.group_id);
 	});
 
 	it("should get the join req in the join req list", async function() {
 		//get the join req from the list
-		const joins = await group2.getJoinRequests();
+		const joins = await connected_group.getJoinRequests();
 
 		chai.assert.equal(joins.length, 1);
 		chai.assert.equal(joins[0].user_id, group1.data.group_id);
@@ -150,19 +150,19 @@ describe("Group test 2", () => {
 	});
 
 	it("should reject the group join req", async function() {
-		await group2.rejectJoinRequest(group1.data.group_id);
+		await connected_group.rejectJoinRequest(group1.data.group_id);
 	});
 
 	it("should send join again to accept it", async function() {
-		await group1.groupJoinRequest(group2.data.group_id);
+		await group1.groupJoinRequest(connected_group.data.group_id);
 	});
 
 	it("should accept join req", async function() {
-		await group2.acceptJoinRequest(group1.data.group_id, 2);
+		await connected_group.acceptJoinRequest(group1.data.group_id, 2);
 	});
 
 	it("should access the group after accepting group join req", async function() {
-		const group_c = await group1.getConnectedGroup(group2.data.group_id);
+		const group_c = await group1.getConnectedGroup(connected_group.data.group_id);
 
 		chai.assert.equal(group_c.data.access_by_group_as_member, group1.data.group_id);
 	});
